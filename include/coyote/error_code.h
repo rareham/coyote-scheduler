27901b191594489920935dcadc9d605f763c1a03
Pantazis Deligiannis <pdeligia@microsoft.com>
2021-01-12 12:22:39 -0800

    move error message description to test (#22)

--- include/coyote/error_code.h
+++ include/coyote/error_code.h
@@ -28,49 +28,6 @@ namespace coyote
         InternalError = 500,
         SchedulerDisabled = 501
     };
-
-    std::string error_message(ErrorCode error_code)
-    {
-        switch (error_code)
-        {
-        case ErrorCode::Success:
-            return "success";
-        case ErrorCode::Failure:
-            return "failure";
-        case ErrorCode::DeadlockDetected:
-            return "deadlock detected";
-        case ErrorCode::DuplicateOperation:
-            return "operation already exists";
-        case ErrorCode::NotExistingOperation:
-            return "operation does not exist";
-        case ErrorCode::MainOperationExplicitlyCreated:
-            return "not allowed to explicitly create main operation '0'";
-        case ErrorCode::MainOperationExplicitlyStarted:
-            return "not allowed to explicitly start main operation '0'";
-        case ErrorCode::MainOperationExplicitlyCompleted:
-            return "not allowed to explicitly complete main operation '0'";
-        case ErrorCode::OperationNotStarted:
-            return "operation has not started";
-        case ErrorCode::OperationAlreadyStarted:
-            return "operation has already started";
-        case ErrorCode::OperationAlreadyCompleted:
-            return "operation has already completed";
-        case ErrorCode::DuplicateResource:
-            return "resource already exists";
-        case ErrorCode::NotExistingResource:
-            return "resource does not exist";
-        case ErrorCode::ClientAttached:
-            return "client is already attached to the scheduler";
-        case ErrorCode::ClientNotAttached:
-            return "client is not attached to the scheduler";
-        case ErrorCode::InternalError:
-            return "internal error";
-        case ErrorCode::SchedulerDisabled:
-            return "scheduler is disabled";
-        default:
-            return "(unknown error)";
-        }
-    }
 }
 
 #endif // COYOTE_ERROR_CODE_H
